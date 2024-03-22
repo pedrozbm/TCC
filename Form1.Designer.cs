2bdd582f7e696bf9772cd8824dcf11e1d5ab9aef
Pedro <Pedrohsb04@gmail.com>
2024-03-22 11:51:24 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TCC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btConectar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btEnviar = new System.Windows.Forms.Button();
+            this.textBoxEnviar = new System.Windows.Forms.TextBox();
+            this.textBoxReceber = new System.Windows.Forms.TextBox();
+            this.timerCOM = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btConectar
+            // 
+            this.btConectar.Location = new System.Drawing.Point(69, 63);
+            this.btConectar.Name = "btConectar";
+            this.btConectar.Size = new System.Drawing.Size(75, 23);
+            this.btConectar.TabIndex = 0;
+            this.btConectar.Text = "Conectar";
+            this.btConectar.UseVisualStyleBackColor = true;
+            this.btConectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(165, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btEnviar
+            // 
+            this.btEnviar.Location = new System.Drawing.Point(69, 117);
+            this.btEnviar.Name = "btEnviar";
+            this.btEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btEnviar.TabIndex = 2;
+            this.btEnviar.Text = "Enviar";
+            this.btEnviar.UseVisualStyleBackColor = true;
+            // 
+            // textBoxEnviar
+            // 
+            this.textBoxEnviar.Location = new System.Drawing.Point(165, 117);
+            this.textBoxEnviar.Name = "textBoxEnviar";
+            this.textBoxEnviar.Size = new System.Drawing.Size(260, 20);
+            this.textBoxEnviar.TabIndex = 3;
+            // 
+            // textBoxReceber
+            // 
+            this.textBoxReceber.Location = new System.Drawing.Point(69, 179);
+            this.textBoxReceber.Multiline = true;
+            this.textBoxReceber.Name = "textBoxReceber";
+            this.textBoxReceber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxReceber.Size = new System.Drawing.Size(356, 142);
+            this.textBoxReceber.TabIndex = 4;
+            // 
+            // timerCOM
+            // 
+            this.timerCOM.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 358);
+            this.Controls.Add(this.textBoxReceber);
+            this.Controls.Add(this.textBoxEnviar);
+            this.Controls.Add(this.btEnviar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btConectar);
+            this.Name = "Form1";
+            this.Text = "Teste de comunicação ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btConectar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btEnviar;
+        private System.Windows.Forms.TextBox textBoxEnviar;
+        private System.Windows.Forms.TextBox textBoxReceber;
+        private System.Windows.Forms.Timer timerCOM;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void atualizalistaCOMs()
+        {
+            int i;
+            bool quantDiferente = false;
+            i = 0;
+
+            if(comboBox1.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                  foreach(string s in SerialPort.GetPortNames())
+                {
+                    if (comboBox1.Items[i++].Equals(s) == false) {
+
+                        quantDiferente = true;
+                    }
+                }
+            }
+            else
+            {
+                quantDiferente = true;
+            }
+
+            if( quantDiferente == false) {
+                return;
+            }
+            comboBox1.Items.Clear();
+
+            foreach(string s in SerialPort.GetPortNames() )
+            {
+                comboBox1.Items.Add(s);
+            }
+            comboBox1.SelectedItem = 0;
+        }
+
+
+    }
+}
